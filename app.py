286cb2f2ad6158a181def6b8ece3ac603d928161
Samarth Samuel <your.email@example.com>
2025-06-16 22:41:50 +0530

    🐛 Fix: Close app.run() properly

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def health():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug
+    app.run(host="0.0.0.0", port=port, debug=True)
